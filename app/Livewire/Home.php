32150ccfa16288a3925a9bb3485e9a1c3e2b09a6
ItsRealNise <anfian284@gmail.com>
2024-07-08 20:29:24 +0700

    post articles

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -3,14 +3,38 @@
 namespace App\Livewire;
 
 use App\Models\Article;
+use Livewire\Attributes\Rule;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 #[Title('Home')]
 class Home extends Component
 {
-    #[\Livewire\Attributes\On('commentAdded')]
+
+    #[Rule('required')]
+    public $title, $content;
+
+    #[\Livewire\Attributes\On('postAdded')]
     public function updateList($articles) {
+    }
+
+    public function save() {
+        $this->validate();
+
+        $myPost = Article::create([
+            'title' => $this->title,
+            'body' => $this->content,
+            'description' => substr($this->content, 0, 100),
+            'slug' => Str::slug($this->title),
+            'user_id' => auth()->id(),
+        ]);
+
+        $this->title = '';
+        $this->content = '';
+
+        $this->reset();
+        $this->dispatch('postAdded', $myPost);
     }
 
     public function render()
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -3,11 +3,39 @@
         <div class="m-6">
             <a href="/articles/{{ $item->slug }}" class="text-2xl font-bold hover:underline">{{ $item->title }}</a>
             <div class="">
-                <span class="text-gray-600">By: <a href="#" class="text-blue-500 hover:underline">{{ $item->user->name }}</a></span>
+                <span class="text-gray-600">By: <a href="#"
+                        class="text-blue-500 hover:underline">{{ $item->user->name }}</a></span>
                 <span class="text-gray-600"> - {{ $item->created_at->diffForHumans() }}</span>
             </div>
             <p class="text-gray-600">{{ $item->description }}</p>
         </div>
         <hr>
     @endforeach
+    <div class="fixed bottom-0 right-0 m-4">
+        <label for="my_modal_7"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:underline cursor-pointer transition">
+            Tambah Post
+        </label>
+    </div>
+    <input type="checkbox" id="my_modal_7" class="modal-toggle" />
+    <div class="modal" role="dialog">
+        <div class="modal-box">
+            <h3 class="text-lg font-bold items-center">Post</h3>
+            <form wire:submit='save'>
+                <div class="mb-4">
+                    <label for="title" class="block text-gray-700 text-sm font-bold mb-2">Title</label>
+                    <input type="text" wire:model='title' id="title"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                </div>
+                <div class="mb-4">
+                    <label for="content" class="block text-gray-700 text-sm font-bold mb-2">Content</label>
+                    <textarea wire:model='content' id="content"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
+                </div>
+                <button type="submit"
+                    class="bg-blue-500 modal-backdrop hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:underline cursor-pointer transition" for="my_modal_7">Submit</button>
+            </form>
+        </div>
+        <label class="modal-backdrop" for="my_modal_7">Close</label>
+    </div>
 </div>
