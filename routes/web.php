29a28a692c785cd3245dbe2b932318b428c1f214
ItsRealNise <anfian284@gmail.com>
2024-07-13 14:55:54 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Livewire\Articles\Index;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Livewire\Chat\Chat;
 
 Route::get('/login', [HomeController::class, 'login'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('loginAuth');
@@ -15,4 +16,5 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
     Route::get('/articles/{slug}', Index::class)->name('articles');
     Route::get('/profile/{id}', Profile::class)->name('profile');
+    Route::get('/chat/{id}', Chat::class)->name('chat');
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui'),
-    require('flowbite/plugin')
-  ],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            gridTemplateColumns: {
+                // Simple 16 column grid
+                '16': 'repeat(16, minmax(0, 1fr))',
+
+                // Complex site-specific column configuration
+                'footer': '200px minmax(900px, 1fr) 100px',
+            }
+        },
+    },
+    plugins: [
+        require('daisyui'),
+        require('flowbite/plugin')
+    ],
 }
 
